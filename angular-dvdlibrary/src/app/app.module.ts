90daf90dcdae53d44cec3356bcbb30090435b90c
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-04 16:23:00 -0500

    Added popup confirmation

--- angular-dvdlibrary/src/app/app.module.ts
+++ angular-dvdlibrary/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- angular-dvdlibrary/src/app/dvd-list/dvd-list.component.ts
+++ angular-dvdlibrary/src/app/dvd-list/dvd-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+//import {MatDialog} from '@angular/material/dialog';
+
+import { DvdService } from '../dvd.service';
 
 @Component({
   selector: 'app-dvd-list',
@@ -8,10 +11,37 @@ import { Component, OnInit, Input } from '@angular/core';
 export class DvdListComponent implements OnInit {
 
   @Input() dvds = [];
-
-  constructor() { }
+  //public dialog: MatDialog, 
+  constructor(private dvdlibrary: DvdService) { }
 
   ngOnInit(): void {
   }
 
+  onDelete(id:number){
+    /*
+    const dialog = "<mat-dialog-content>Are you sure you want to delete?</mat-dialog-content>"
+      + "<mat-dialog-actions>"
+      + "<button mat-button mat-dialog-close>Cancel</button>"
+      + "<button mat-button [mat-dialog-close]='true'>Delete</button>"
+      + "</mat-dialog-actions>"
+    const dialogRef = this.dialog.open(dialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result === 'true'){
+        this.dvdlibrary.deleteDvd(id)
+        .subscribe(
+          response => console.log(response),
+          err => console.log(err)
+         );
+      }
+    });*/
+
+    if(confirm("Are you sure to delete DVD?")){
+      this.dvdlibrary.deleteDvd(id)
+        .subscribe(
+          response => console.log(response),
+          err => console.log(err)
+         );
+    }
+  }
 }
