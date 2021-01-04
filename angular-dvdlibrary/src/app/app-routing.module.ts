9acf0f61981e8028ed67e0824e2128edbc7f65e1
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-03 22:11:50 -0500

    setup routing

--- angular-dvdlibrary/src/app/app-routing.module.ts
+++ angular-dvdlibrary/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'edit', loadChildren: () => import('./edit/edit.module').then(m => m.EditModule) }, { path: 'add', loadChildren: () => import('./add/add.module').then(m => m.AddModule) }, { path: 'detail', loadChildren: () => import('./detail/detail.module').then(m => m.DetailModule) }];
+const routes: Routes = [
+  {
+    path: 'edit',
+    loadChildren: () => import('./edit/edit.module').then(m => m.EditModule)
+  },{
+    path: 'add',
+    loadChildren: () => import('./add/add.module').then(m => m.AddModule)
+  }, {
+    path: 'detail',
+    loadChildren: () => import('./detail/detail.module').then(m => m.DetailModule)
+  }, {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
